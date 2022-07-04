8208021a3e6bdabc424135acf67556bf20459cb8
hggmarks <hggmarks@gmail.com>
2022-07-03 22:33:04 -0300

    mergeSort first try

--- sorting/merge-sort/mergeSort.cpp
+++ sorting/merge-sort/mergeSort.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 
 using std::cout;
+using std::cin;
 using std::endl;
 
-void merge(int arr[], int low, int mid, int high) {
+
+/* void merge(int arr[], int low, int high) {
     int i = 0, j = 0, k = 0;
 
+    int mid = low + (high - low)/2;
     int firstHalf[mid - low + 1];
     int secondHalf[mid - high];
 
@@ -18,24 +21,86 @@ void merge(int arr[], int low, int mid, int high) {
         }
     }
 
-    while ((i < mid) && (j < high)) {
-        if ( arr[i] <= arr[mid + j] )
+    int p = mid - low + 1;
+    int q = high - mid;
+    
+
+    while ((i < p) && (j < q)) {
+        if (firstHalf[i] < secondHalf[j]) {
+            arr[k] = firstHalf[i];
+            i++;
+        } else {
+            arr[k] = secondHalf[j];
+            j++
+        }
+        k++;
+    }
+    
+    if (i == p) {
+
+    }
+} */
+
+void merge(int arr[], int low, int high) {
+    int tempArr[high - low];
+
+    for( int count = low; count < high; count++ ) {
+        tempArr[count] = arr[count];
+    }
+
+    int mid = low + (high - low)/2;
+
+    int i = low;
+    int j = mid + 1;
+
+    for (int cursor = low; cursor < high; cursor++) {
+        if ( tempArr[i] <= tempArr[j] ) {
+            arr[cursor] = tempArr[i++];
+        } else if ( tempArr[i] > tempArr[j] ) {
+            arr[cursor] = tempArr[j++];
+        } else if ( i == (mid+1) ) {
+            arr[cursor] = tempArr[j++];
+        } else if ( j > high ) {
+            arr[cursor] = tempArr[i++];
+        }
     }
 }
 
 void mergeSort(int arr[], int low, int high) {
 
-    int arrSize = sizeof(&arr) / sizeof(arr[0]);
-    if ( arrSize > 1 ) {
-        mergeSort(arr, low, arrSize/2);
-        mergeSort(arr, (arrSize/2)+1, high);
-        merge(arr, low, arrSize/2, high);
+    if ( low < high ) {
+
+        int mid = low + (high - low)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, high);
     }
 }
 
+void printArray(int arr[], int arrSize){
+    for(int i = 0; i < arrSize; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
 int main() {
-    int test[] = {1,2,3};
-    cout << sizeof(test)/sizeof(test[0]) << endl;
+    int totalCases = 0, arrSize = 0;
+    
+    cin >> totalCases;
+
+    for(int i = 0; i < totalCases; i++) {
+        cin >> arrSize;
+        int arr[arrSize];
+
+        for(int j = 0; j < arrSize; j++) {
+            cin >> arr[j];
+        }
+
+        mergeSort(arr, 0, arrSize - 1);
+        printArray(arr, arrSize);
+        
+    }
 
     return 0;
 }
