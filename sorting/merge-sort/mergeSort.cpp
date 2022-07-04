fa72adc40b0f6b3c03ec5348f1fd6a51f05f9d59
Hugo Marques <hfm@discente.ifpe.edu.br>
2022-07-04 18:06:29 +0000

    merge sort fixed

--- sorting/merge-sort/mergeSort.cpp
+++ sorting/merge-sort/mergeSort.cpp
@@ -5,46 +5,11 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-
-/* void merge(int arr[], int low, int high) {
-    int i = 0, j = 0, k = 0;
-
-    int mid = low + (high - low)/2;
-    int firstHalf[mid - low + 1];
-    int secondHalf[mid - high];
-
-    for (int count = 0; count <= high; ++count) {
-        if (count <= mid) {
-            firstHalf[count] = arr[count];
-        } else {
-            secondHalf[count % mid] = arr[count];
-        }
-    }
-
-    int p = mid - low + 1;
-    int q = high - mid;
-    
-
-    while ((i < p) && (j < q)) {
-        if (firstHalf[i] < secondHalf[j]) {
-            arr[k] = firstHalf[i];
-            i++;
-        } else {
-            arr[k] = secondHalf[j];
-            j++
-        }
-        k++;
-    }
-    
-    if (i == p) {
-
-    }
-} */
-
 void merge(int arr[], int low, int high) {
-    int tempArr[high - low];
+        
+    int tempArr[high + 1];
 
-    for( int count = low; count < high; count++ ) {
+    for( int count = low; count <= high; count++ ) {
         tempArr[count] = arr[count];
     }
 
@@ -53,16 +18,18 @@ void merge(int arr[], int low, int high) {
     int i = low;
     int j = mid + 1;
 
-    for (int cursor = low; cursor < high; cursor++) {
-        if ( tempArr[i] <= tempArr[j] ) {
-            arr[cursor] = tempArr[i++];
-        } else if ( tempArr[i] > tempArr[j] ) {
-            arr[cursor] = tempArr[j++];
-        } else if ( i == (mid+1) ) {
+    for (int cursor = low; cursor <= high; cursor++) {
+
+        if ( i == (mid+1) ) {
             arr[cursor] = tempArr[j++];
         } else if ( j > high ) {
             arr[cursor] = tempArr[i++];
+        } else if ( tempArr[i] <= tempArr[j] ) {
+            arr[cursor] = tempArr[i++];
+        } else if ( tempArr[i] > tempArr[j] ) {
+            arr[cursor] = tempArr[j++];
         }
+
     }
 }
 
