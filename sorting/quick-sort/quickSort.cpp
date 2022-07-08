1595dadf2cad0f7494bebbeae102932535b364c5
Hugo Marques <hfm@cin.ufpe.br>
2022-07-08 19:51:00 +0000

    started implementation of quick-sort

---
+++ sorting/quick-sort/quickSort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void quickSort(int arr[], int low, int high) {
+    
+}
+
+void printArray(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
+int main(){
+
+}
