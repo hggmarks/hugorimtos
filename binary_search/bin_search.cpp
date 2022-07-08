4c643eec2d19ed9a69ce648f86523a10926efd73
Hugo Marques <hfm@cin.ufpe.br>
2022-07-08 19:28:48 +0000

    cpp version of binary search

---
+++ binary_search/bin_search.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int binSearch(int sortedArr[], int low, int high, int term){
+
+    int idx = 0, mid = 0;
+
+    if (low <= high) {
+
+        mid = low + (high - low)/2;
+    
+        if (term == sortedArr[mid]) {
+            return mid;
+
+        } else if (term > sortedArr[mid]) {
+            return binSearch(sortedArr, mid+1, high, term);
+        }
+
+        return binSearch(sortedArr, low, mid-1, term);
+    } else {
+        return -1;
+    }
+}
+
+void printArray(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    int arrSize = 0, totalNumbers = 0, num = 0;
+
+    cin >> arrSize;
+
+    int sortedArr[arrSize];
+
+    for (int i = 0; i < arrSize; i++) {
+        cin >> sortedArr[i];
+    }
+
+    cin >> totalNumbers;
+
+    for (int j = 0; j < totalNumbers; j++) {
+        cin >> num;
+        
+        int result = binSearch(sortedArr, 0, arrSize - 1, num);
+
+        result != -1 ? cout << result << endl : cout << 'x' << endl;
+    }
+
+    return 1;
+}
