516c132c93c03696f99acdb6c7bdb56afee5feb0
Hugo Marques <hfm@cin.ufpe.br>
2022-07-16 06:27:16 +0000

    started solving problem B

---
+++ problems/B-Hacking-the-random-number-generator/hacking-the-random-number-generator.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+
+void tempSwap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int hoarePartition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low, j = high + 1;
+
+    do {
+        do {
+            i++;
+        } while(arr[i] < pivot);
+
+        do {
+            j--;
+        } while (arr[j] > pivot);
+        tempSwap(&arr[i], &arr[j]);
+    } while(i < j);
+    
+    tempSwap(&arr[i], &arr[j]);
+    tempSwap(&arr[j], &arr[low]);
+
+    return j;
+}
+
+void quickSort(int arr[], int low, int high) {
+
+    int splitPos = hoarePartition(arr, low, high);
+    quickSort(arr, low, splitPos-1);
+    quickSort(arr, splitPos+1, high);
+}
+
+int binarySearch(int arr[], int low, int high, int term) {
+    int idx = 0;
+    if (low <= high) {
+        int mid = low + (high - low)/2;
+
+        if (term == arr[mid]){
+            return 1;
+        } else if (term > arr[mid]) {
+            return binarySearch(arr, mid+1, high, term);
+        }
+
+        return binarySearch(arr, low, mid-1, term);
+
+    } else return 0;
+}
+
+void fillArray(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        cin >> arr[i];
+    }
+}
+
+int main() {
+    int arrSize, k, totalNum = 0;
+
+    cin >> arrSize;
+    cin >> k;
+
+    int arr[arrSize];
+
+    for(int j = 0; j < arrSize; j++) {
+            cin >> arr[j];
+        }
+
+    quickSort(arr, 0, arrSize-1);
+
+    for(int i = 0; i < arrSize; i++){
+        totalNum += binarySearch(arr, 0, arrSize-1, arr[i]+k);
+    }
+    
+    cout << totalNum;
+    
+    return 0;
+}
