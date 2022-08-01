59cc9950f293cae6e9d67eecf7840df768e9f9ca
Hugo Marques <hfm@cin.ufpe.br>
2022-08-01 18:28:52 +0000

    implemented cursor movement methods, and some insertion/remove methods

--- data-structures/lists/Alist.cpp
+++ data-structures/lists/Alist.cpp
@@ -1,5 +1,8 @@
 #include "list.h"
+#include <bits/stdc++.h>
 
+using std::cout;
+using std::endl;
 
 template<typename E>
 class AList: public List<E> {
@@ -26,5 +29,48 @@ class AList: public List<E> {
             listSize = cursor = 0;
             listArr = new E[maxArrSize];
         }
+
+        void insert(const E& element){
+            if (listSize < maxArrSize){
+                for(int i = listSize; i < cursor; i--){
+                    listArr[i] = listArr[i-1];
+                }
+                listArr[cursor] = element;
+                listSize++;
+            } else {
+                cout << "the list is full, cannot insert elements" << endl;
+            }
+        }
+
+        void append(const E& element){
+            if(listSize < maxArrSize){
+                listArr[listSize++] = element;
+            } else {
+                cout << "the list is full, cannot append elements" << endl;
+            }
+        }
+
+        E remove(){
+            if( (cursor >=0) && (cursor < listSize) ){
+                E element = listArr[cursor];
+
+                for (int i = cursor; i<listSize-1; i++){
+                    listArr[i] = listArr[i+1];
+                }
+                listSize--;
+                return element;
+            }
+        }
+
+        void moveToStart(){ cursor = 0; }
+        void moveToEnd(){ cursor = listSize; }
+        void prev(){ if (cursor > 0) cursor--; }
+        void next(){ if (cursor < listSize) cursor++; }
+
+        int lenght() const { return listSize; }
+
+        int cursorPos() const { return cursor; }
+
+
 
 };
