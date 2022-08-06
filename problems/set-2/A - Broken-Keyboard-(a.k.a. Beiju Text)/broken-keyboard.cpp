8f6f6b860657e026149b9d896659d28f9f6420dd
Hugo Marques <hfm@cin.ufpe.br>
2022-08-06 21:41:59 +0000

    organized problems

---
+++ problems/set-2/A - Broken-Keyboard-(a.k.a. Beiju Text)/broken-keyboard.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int strSize = 0;
+    string inputText = "";
+    cin >> inputText;
+
+    for (int i = 0; inputText[i] != "/0"; i++)
+
+    
+
+}
